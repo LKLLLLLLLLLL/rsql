66864943671a2b6dece2b147a1c98453ae5e42b1
ZYM <1534628507@qq.com>
2026-01-13 22:19:43 +0800

    For merge

--- src/db/parser.rs
+++
@@ -1,5 +0,0 @@
-mod parser;
-
-use sqlparser::dialect::GenericDialect;
-use sqlparser::parser::Parser;
-use parser::ast_to_plan::build_logical_plan;
--- src/db/parser/mod.rs
+++
@@ -1,7 +0,0 @@
-/// Parser module for SQL query processing.
-/// This module contains submodules for building logical plans, expression utilities, and pretty printing.
-
-pub mod ast_to_plan;
-pub mod expr_utils;
-pub mod logical_plan;
-pub mod pretty_print;
